1e9f9caccf1f27b4c8a99e976f77cd6b808386ae
Rose <skillwarzaking@gmail.com>
2025-08-06 16:21:44 +0200

    Finished aspis.

--- src/main/java/net/rose/steamporium/common/item/EnduriumAspisItem.java
+++ src/main/java/net/rose/steamporium/common/item/EnduriumAspisItem.java
@@ -1,16 +1,20 @@
 package net.rose.steamporium.common.item;
 
+import net.fabric_extras.shield_api.item.CustomShieldItem;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.text.MutableText;
-import net.minecraft.item.ShieldItem;
 import net.minecraft.text.Text;
+import net.rose.steamporium.common.init.ModItems;
 
-public class EnduriumAspisItem extends ShieldItem {
+import java.util.List;
+
+public class EnduriumAspisItem extends CustomShieldItem {
     public EnduriumAspisItem(Settings settings) {
-        super(settings);
+        super(null, () -> Ingredient.ofItems(ModItems.ENDURIUM_ALLOY), List.of(), settings);
     }
 
     @Override
     public Text getName() {
-        return ((MutableText)super.getName()).styled(style -> style.withColor(0xFEEEBA));
+        return ((MutableText) super.getName()).styled(style -> style.withColor(0xFEEEBA));
     }
 }
